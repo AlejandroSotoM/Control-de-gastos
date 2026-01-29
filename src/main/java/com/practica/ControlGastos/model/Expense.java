f8ac86df53440bfdf353ad0fcbb3ebe25550ca4b
Alejandro <asotomiguel11@gmail.com>
2026-01-28 22:32:17 -0600

    Add category and payment method enums

--- src/main/java/com/practica/ControlGastos/model/Expense.java
+++ src/main/java/com/practica/ControlGastos/model/Expense.java
@@ -1,16 +1,88 @@
 package com.practica.ControlGastos.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Entity
 public class Expense {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column()
     private String description;
     private BigDecimal amount;
 
+    @Enumerated(EnumType.STRING)
+    private CategoryEnum category;
+
     private LocalDate date;
 
+    @Enumerated(EnumType.STRING)
+    private PaymentMethodEnum paymentMethod;
+
+    //Constructor vacio para JPA
+    public Expense(){}
+
+    //Constructor
+    public Expense(Long id, String description, BigDecimal amount, CategoryEnum category, LocalDate date, PaymentMethodEnum paymentMethod) {
+        this.id = id;
+        this.description = description;
+        this.amount = amount;
+        this.category = category;
+        this.date = date;
+        this.paymentMethod = paymentMethod;
+    }
+
+    //Getters and Setters
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public CategoryEnum getCategory() {
+        return category;
+    }
+
+    public void setCategory(CategoryEnum category) {
+        this.category = category;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public PaymentMethodEnum getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(PaymentMethodEnum paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
 }
--- src/main/java/com/practica/ControlGastos/model/PaymentMethodEnum.java
+++ src/main/java/com/practica/ControlGastos/model/PaymentMethodEnum.java
@@ -1,11 +1,10 @@
 package com.practica.ControlGastos.model;
 
-public class PaymentMethodEnum {
-    private enum paymentMethod{
-        CASH,
-        DEBIT_CARD,
-        CREDIT_CARD,
-        BANK_TRANSFER,
-        DIGITAL_WALLET
-    };
+public enum PaymentMethodEnum {
+    CASH,
+    DEBIT_CARD,
+    CREDIT_CARD,
+    BANK_TRANSFER,
+    DIGITAL_WALLET
+
 }
