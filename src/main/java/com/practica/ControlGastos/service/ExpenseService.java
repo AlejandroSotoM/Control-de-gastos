c10cf74d16f3b5347a8ba215abc2233e38d11307
Alejandro <asotomiguel11@gmail.com>
2026-01-30 22:42:44 -0600

    ExpenseService correction

--- src/main/java/com/practica/ControlGastos/service/ExpenseService.java
+++ src/main/java/com/practica/ControlGastos/service/ExpenseService.java
@@ -1,13 +1,9 @@
 package com.practica.ControlGastos.service;
 
 import com.practica.ControlGastos.model.Expense;
-import com.practica.ControlGastos.repository.ExpenseRepository;
-import org.hibernate.tool.schema.spi.ExecutionOptions;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
 public interface ExpenseService {
 
     Expense create(Expense expense);
